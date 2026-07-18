3ddd433db07a6a7b1ceaaf940aeef66ae0ccaac1
Oya Gecco <oyagecco@yahoo.com>
2026-07-18 11:43:58 +0300

    Acquire EFI filesystem protocol

--- boot/src/filesystem.c
+++ boot/src/filesystem.c
@@ -1,9 +1,27 @@
 #include <filesystem.h>
 
-void filesystem_init(
+static EFI_BOOT_SERVICES *BootServices;
+
+EFI_STATUS filesystem_init(
     EFI_HANDLE ImageHandle,
     EFI_SYSTEM_TABLE *SystemTable)
 {
-    (void)ImageHandle;
-    (void)SystemTable;
+    BootServices = SystemTable->BootServices;
+
+    EFI_LOADED_IMAGE_PROTOCOL *LoadedImage;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *FileSystem;
+
+    EFI_STATUS Status = BootServices->HandleProtocol(
+        ImageHandle,
+        &EFI_LOADED_IMAGE_PROTOCOL_GUID,
+        (void **)&LoadedImage
+    );
+
+    Status = BootServices->HandleProtocol(
+        LoadedImage->DeviceHandle,
+        &EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID,
+        (void **)&FileSystem
+    );
+
+    return Status;
 }
