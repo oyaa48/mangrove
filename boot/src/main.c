20b28459eb0007875e285199ac8686ae189b72d2
Oya Gecco <oyaa48@icloud.com>
2026-07-18 03:14:46 +0300

    Print first message from UEFI bootloader

--- boot/src/main.c
+++ boot/src/main.c
@@ -5,7 +5,15 @@ EFI_STATUS EFIAPI efi_main(
     EFI_SYSTEM_TABLE *SystemTable)
 {
     (void)ImageHandle;
-    (void)SystemTable;
+
+    SystemTable->ConOut->OutputString(
+        SystemTable->ConOut,
+        L"Mangrove Boot\r\n"
+    );
+
+    for (;;)
+    {
+    }
 
     return EFI_SUCCESS;
 }
