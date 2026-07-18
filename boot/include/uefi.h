198bc192db9bbb9f0e7838c566a0c74f11c3e191
Oya Gecco <oyagecco@yahoo.com>
2026-07-18 03:14:46 +0300

    Print first message from UEFI bootloader

--- boot/include/uefi.h
+++ boot/include/uefi.h
@@ -2,8 +2,11 @@
 
 #include <types.h>
 
+/* Basic types */
+
 typedef u64 EFI_STATUS;
 typedef void *EFI_HANDLE;
+typedef u16 CHAR16;
 
 #define EFI_SUCCESS 0
 
@@ -15,11 +18,74 @@ typedef void *EFI_HANDLE;
 
 /* Forward declarations */
 
+typedef struct EFI_TABLE_HEADER EFI_TABLE_HEADER;
+
 typedef struct EFI_SYSTEM_TABLE EFI_SYSTEM_TABLE;
 
-/* Entry point */
+typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+typedef struct EFI_RUNTIME_SERVICES
+    EFI_RUNTIME_SERVICES;
+
+typedef struct EFI_BOOT_SERVICES
+    EFI_BOOT_SERVICES;
+
+typedef struct EFI_CONFIGURATION_TABLE
+    EFI_CONFIGURATION_TABLE;
+
+/* Function pointers */
 
 typedef EFI_STATUS (EFIAPI *EFI_IMAGE_ENTRY_POINT)(
     EFI_HANDLE ImageHandle,
     EFI_SYSTEM_TABLE *SystemTable
 );
+
+typedef EFI_STATUS (EFIAPI *EFI_TEXT_STRING)(
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This,
+    CHAR16 *String
+);
+
+/* Structures */
+
+struct EFI_TABLE_HEADER
+{
+    u64 Signature;
+    u32 Revision;
+    u32 HeaderSize;
+    u32 CRC32;
+    u32 Reserved;
+};
+
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+{
+    void *Reset;
+    EFI_TEXT_STRING OutputString;
+};
+
+struct EFI_SYSTEM_TABLE
+{
+    EFI_TABLE_HEADER Hdr;
+
+    CHAR16 *FirmwareVendor;
+    u32 FirmwareRevision;
+
+    EFI_HANDLE ConsoleInHandle;
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL *ConIn;
+
+    EFI_HANDLE ConsoleOutHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+
+    EFI_HANDLE StandardErrorHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *StdErr;
+
+    EFI_RUNTIME_SERVICES *RuntimeServices;
+    EFI_BOOT_SERVICES *BootServices;
+
+    usize NumberOfTableEntries;
+
+    EFI_CONFIGURATION_TABLE *ConfigurationTable;
+};
