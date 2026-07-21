44a669386eb93e1337ad13cca9bb1f6c3206256c
Oya Gecco <oyaa48@icloud.com>
2026-07-21 11:44:24 +0300

    Add hardware IRQ support

--- kernel/src/idt_asm.s
+++ kernel/src/idt_asm.s
@@ -1,6 +1,8 @@
 .global idt_load
 .global isr_stub_table
+.global irq_stub_table
 .extern exception_handler
+.extern irq_handler
 
 idt_load:
     lidt (%rdi)
@@ -9,8 +11,8 @@ idt_load:
 .macro ISR_NOERRCODE num
 .global isr_\num
 isr_\num:
-    pushq $0                  # Push dummy error code (0)
-    pushq $\num               # Push vector number
+    pushq $0
+    pushq $\num
     jmp common_isr_stub
 .endm
 
@@ -54,6 +56,31 @@ ISR_ERRCODE   29
 ISR_ERRCODE   30
 ISR_NOERRCODE 31
 
+.macro IRQ num
+.global irq_\num
+irq_\num:
+    pushq $0
+    pushq $(32 + \num)
+    jmp common_irq_stub
+.endm
+
+IRQ 0
+IRQ 1
+IRQ 2
+IRQ 3
+IRQ 4
+IRQ 5
+IRQ 6
+IRQ 7
+IRQ 8
+IRQ 9
+IRQ 10
+IRQ 11
+IRQ 12
+IRQ 13
+IRQ 14
+IRQ 15
+
 common_isr_stub:
     pushq %rax
     pushq %rbx
@@ -72,7 +99,56 @@ common_isr_stub:
     pushq %r15
 
     movq %rsp, %rdi
+    movq %rsp, %rbx
+    andq $-16, %rsp
+    subq $8, %rsp
+    pushq %rbx
     call exception_handler
+    popq %rsp
+
+    popq %r15
+    popq %r14
+    popq %r13
+    popq %r12
+    popq %r11
+    popq %r10
+    popq %r9
+    popq %r8
+    popq %rbp
+    popq %rdi
+    popq %rsi
+    popq %rdx
+    popq %rcx
+    popq %rbx
+    popq %rax
+
+    addq $16, %rsp
+    iretq
+
+common_irq_stub:
+    pushq %rax
+    pushq %rbx
+    pushq %rcx
+    pushq %rdx
+    pushq %rsi
+    pushq %rdi
+    pushq %rbp
+    pushq %r8
+    pushq %r9
+    pushq %r10
+    pushq %r11
+    pushq %r12
+    pushq %r13
+    pushq %r14
+    pushq %r15
+
+    movq %rsp, %rdi
+    movq %rsp, %rbx
+    andq $-16, %rsp
+    subq $8, %rsp
+    pushq %rbx
+    call irq_handler
+    popq %rsp
 
     popq %r15
     popq %r14
@@ -99,3 +175,9 @@ isr_stub_table:
     .quad isr_8,  isr_9,  isr_10, isr_11, isr_12, isr_13, isr_14, isr_15
     .quad isr_16, isr_17, isr_18, isr_19, isr_20, isr_21, isr_22, isr_23
     .quad isr_24, isr_25, isr_26, isr_27, isr_28, isr_29, isr_30, isr_31
+
+irq_stub_table:
+    .quad irq_0,  irq_1,  irq_2,  irq_3
+    .quad irq_4,  irq_5,  irq_6,  irq_7
+    .quad irq_8,  irq_9,  irq_10, irq_11
+    .quad irq_12, irq_13, irq_14, irq_15
