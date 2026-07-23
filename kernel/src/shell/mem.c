f1e0768585d756d818e2214e079b281655218a93
Oya Gecco <oyaa48@icloud.com>
2026-07-23 14:05:16 +0300

    feat: expose physical memory statistics

--- kernel/src/shell/mem.c
+++ kernel/src/shell/mem.c
@@ -1,5 +1,16 @@
 #include <shell/mem.h>
+#include <units.h>
+#include <pmm.h>
+#include <kprint.h>
 
-void shell_mem(void)
-{
+void shell_mem(void){
+    u64 total = pmm_get_total_memory();
+    u64 used = pmm_get_used_memory();
+    u64 free = pmm_get_free_memory();
+
+    kprint("Memory Information\n\n");
+
+    kprint("Total: %u MiB\n", (u32)bytes_to_mib(total));
+    kprint("Used: %u MiB\n", (u32)bytes_to_mib(used));
+    kprint("Free: %u MiB\n", (u32)bytes_to_mib(free));
 }
