514886d7e3e12017cb4de619a83b74ff79624e84
Oya Gecco <oyaa48@icloud.com>
2026-07-23 09:47:21 +0300

    shell: introduce command framework

---
+++ kernel/src/shell/core.c
@@ -0,0 +1,44 @@
+#include <shell/core.h>
+#include <shell/builtin.h>
+
+#include <shell/help.h>
+#include <shell/version.h>
+#include <shell/clear.h>
+#include <shell/mem.h>
+#include <shell/uptime.h>
+
+#include <kprint.h>
+#include <terminal.h>
+
+#include <string.h>
+
+static const builtin_t builtins[] = {
+    { "help",    shell_help    },
+    { "version", shell_version },
+    { "clear",   shell_clear   },
+    { "mem",     shell_mem     },
+    { "uptime",  shell_uptime  },
+};
+
+void shell_init(void) {
+    kprint("Welcome to Mangrove OS!\n");
+    kprint("Type 'help' to get started.\n\n");
+
+    kprint("> ");
+}
+
+void shell_execute(const char *command)
+{
+    usize count = sizeof(builtins) / sizeof(builtins[0]);
+
+    for (usize i = 0; i < count; i++) {
+        if (strcmp(command, builtins[i].name) == 0) {
+            builtins[i].handler();
+            kprint("> ");
+            return;
+        }
+    }
+
+    kprint("Unknown command.\n");
+    kprint("> ");
+}
