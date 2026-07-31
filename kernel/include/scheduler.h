3cfde887f8a6abd2a5886ecd0081a658c8ca77a6
Oya Gecco <oyaa48@icloud.com>
2026-07-31 17:46:07 +0300

    scheduler: add blocking and idle thread

--- kernel/include/scheduler.h
+++ kernel/include/scheduler.h
@@ -35,6 +35,8 @@ struct kernel_thread {
     u64 default_time_slice;
     uintptr_t preempt_return_rip;
     u64 preempt_return_rflags;
+    u64 wakeup_tick;
+    bool sleeping;
     uintptr_t saved_stack_pointer;
     uintptr_t kernel_stack_base;
     usize kernel_stack_size;
@@ -60,6 +62,9 @@ kernel_thread_t *scheduler_select_next(void);
 bool scheduler_reschedule(void);
 bool scheduler_yield(void);
 bool scheduler_timer_tick(void);
+bool scheduler_block(void);
+bool scheduler_unblock(kernel_thread_t *thread);
+bool scheduler_sleep(u64 ticks);
 bool scheduler_prepare_preemption(struct cpu_registers *regs);
 u64 scheduler_preempt_from_trampoline(void);
 u64 scheduler_preempt_return_flags(void);
